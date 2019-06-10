836a3a5234c38e93929e457d925c1dc70e9b4dec
Dave Moten <davidmoten@gmail.com>
2019-06-10 19:04:16 +1000

    organize imports

--- src/main/java/com/github/davidmoten/shi/HilbertIndex.java
+++ src/main/java/com/github/davidmoten/shi/HilbertIndex.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UncheckedIOException;
 import java.util.Arrays;
 import java.util.TreeMap;
 import java.util.function.Function;
