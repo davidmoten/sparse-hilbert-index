4cec511ac273e0d610c67056ef3d8ffe355f2732
David Moten <davidmoten@gmail.com>
2019-06-12 16:49:51 +1000

    increase coverage

--- src/test/java/com/github/davidmoten/shi/LimitingInputStreamTest.java
+++ src/test/java/com/github/davidmoten/shi/LimitingInputStreamTest.java
@@ -11,38 +11,41 @@ public class LimitingInputStreamTest {
 
     @Test
     public void test() throws IOException {
-        LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 2);
-        assertEquals(0, in.read());
-        assertEquals(0, in.read());
-        assertEquals(-1, in.read());
+        try (LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 2)) {
+            assertEquals(0, in.read());
+            assertEquals(0, in.read());
+            assertEquals(-1, in.read());
+        }
     }
 
     @Test
     public void test2() throws IOException {
-        LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[2]), 100);
-        assertEquals(0, in.read());
-        assertEquals(0, in.read());
-        assertEquals(-1, in.read());
+        try (LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[2]), 100)) {
+            assertEquals(0, in.read());
+            assertEquals(0, in.read());
+            assertEquals(-1, in.read());
+        }
     }
 
     @Test
     public void test3() throws IOException {
-        LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 2);
-        byte[] b = new byte[1];
-        assertEquals(1, in.read(b));
-        assertEquals(1, in.read(b));
-        assertEquals(-1, in.read(b));
-        assertEquals(-1, in.read(b));
+        try (LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 2)) {
+            byte[] b = new byte[1];
+            assertEquals(1, in.read(b));
+            assertEquals(1, in.read(b));
+            assertEquals(-1, in.read(b));
+            assertEquals(-1, in.read(b));
+        }
     }
 
     @Test
     public void test4() throws IOException {
-        LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 9);
-        byte[] b = new byte[5];
-        assertEquals(5, in.read(b));
-        assertEquals(4, in.read(b));
-        assertEquals(-1, in.read(b));
-        assertEquals(-1, in.read(b));
+        try (LimitingInputStream in = new LimitingInputStream(new ByteArrayInputStream(new byte[100]), 9)) {
+            byte[] b = new byte[5];
+            assertEquals(5, in.read(b));
+            assertEquals(4, in.read(b));
+            assertEquals(-1, in.read(b));
+            assertEquals(-1, in.read(b));
+        }
     }
-
 }
