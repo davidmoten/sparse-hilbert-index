faf3ee9eea97d19e946c68fd6623277745170d35
Dave Moten <davidmoten@gmail.com>
2019-06-15 13:02:50 +1000

    add tests

---
+++ src/test/java/com/github/davidmoten/shi/WithStatsTest.java
@@ -0,0 +1,23 @@
+package com.github.davidmoten.shi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class WithStatsTest {
+
+    @Test
+    public void testGetters() {
+        WithStats<Integer> w = new WithStats<>(1, 100, 200, 123, 456, 6, 890);
+        assertTrue(w.hasValue());
+        assertEquals(1, (int) w.value());
+        assertEquals(100, (int) w.recordsRead());
+        assertEquals(200, (int) w.recordsFound());
+        assertEquals(123, (int) w.bytesRead());
+        assertEquals(456, (int) w.timeToFirstByteMs());
+        assertEquals(76, (int) w.timeToFirstByteMsAverage());
+        assertEquals(6, (int) w.chunksRead());
+    }
+
+}
