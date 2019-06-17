b273e7eb57c470c037c9a41a9c93650a43bc32ac
Dave Moten <davidmoten@gmail.com>
2019-06-17 20:24:32 +1000

    close resources in test

--- src/test/java/com/github/davidmoten/shi/CountingInputStreamTest.java
+++ src/test/java/com/github/davidmoten/shi/CountingInputStreamTest.java
@@ -13,15 +13,15 @@ public class CountingInputStreamTest {
 
     @Test
     public void test() throws IOException {
-
-        CountingInputStream in = new CountingInputStream(
-                new ByteArrayInputStream("ab".getBytes(StandardCharsets.UTF_8)), 1000);
-        assertEquals(97, in.read());
-        long x = in.readTimeToFirstByteAndSetToZero();
-        assertTrue(x > 100000);
-        assertEquals(98, in.read());
-        assertEquals(0, in.readTimeToFirstByteAndSetToZero());
-        assertEquals(-1, in.read());
+        try (CountingInputStream in = new CountingInputStream(
+                new ByteArrayInputStream("ab".getBytes(StandardCharsets.UTF_8)), 1000)) {
+            assertEquals(97, in.read());
+            long x = in.readTimeToFirstByteAndSetToZero();
+            assertTrue(x > 100000);
+            assertEquals(98, in.read());
+            assertEquals(0, in.readTimeToFirstByteAndSetToZero());
+            assertEquals(-1, in.read());
+        }
     }
 
 }
